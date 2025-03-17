34cb55ce3aa0db8925bfeadb06c147946f7ec781
DocFoxHound <martin.medic@hotmail.com>
2025-03-17 22:03:54 +0300

    small fix

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -9,12 +9,12 @@ router.get('/', classController.getAllClasses);
 router.get('/:id', classController.getClassById);
 
 // POST request for creating a new user
-router.post('/', userController.createClass);
+router.post('/', classController.createClass);
 
 // PUT request for updating an existing user by ID
-router.put('/:id', userController.updateClass);
+router.put('/:id', classController.updateClass);
 
 // DELETE request for deleting a user by ID
-router.delete('/:id', userController.deleteClass);
+router.delete('/:id', classController.deleteClass);
 
 module.exports = router;
