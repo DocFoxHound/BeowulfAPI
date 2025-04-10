1f2023605f7b60858568fa79ebcd61651763a647
DocFoxHound <martin.medic@hotmail.com>
2025-04-10 20:42:50 +0300

    uploading deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/BeowulfAPI
+
+# Pull the latest code from GitHub
+git pull origin main
+
+# Install any new dependencies
+npm install
+
+# Restart the bot using PM2
+pm2 restart BeowulfApi
+
+# Save the PM2 process list (so it will restart on server reboot)
+pm2 save
