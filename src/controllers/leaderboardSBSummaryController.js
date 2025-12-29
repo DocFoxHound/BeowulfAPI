ee29775580e4494671efa019a495fd34ebb96bce
DocFoxHound <martin.medic@hotmail.com>
2025-12-28 21:58:11 -0700

    fix for adblockers

--- src/controllers/leaderboardSBSummaryController.js
+++ src/controllers/leaderboardSBSummaryController.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/database'); // Use sequelize, not pool
 // GET all player summaries
 exports.getAllPlayerSummaries = async (req, res) => {
     try {
-        const result = await sequelize.query('SELECT * FROM leaderboard_sb_player_summary', { type: sequelize.QueryTypes.SELECT });
+        const result = await sequelize.query('SELECT * FROM `leaderboard_sb_player_summary`', { type: sequelize.QueryTypes.SELECT });
         res.status(200).json(result);
     } catch (error) {
         res.status(500).send(error.message);
